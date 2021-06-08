18115a1ba80e1fa17569a78cbbc6294174f379a7
Pho3b <andrea.rinaldi54@gmail.com>
2021-06-09 00:17:34 +0200

    master: began the implementation of the HashTableOpenAddressing

--- src/main/java/studying/data_structures/hash_table/HashTableOpenAddressing.java
+++ src/main/java/studying/data_structures/hash_table/HashTableOpenAddressing.java
@@ -1,6 +1,36 @@
 package studying.data_structures.hash_table;
 
 public class HashTableOpenAddressing<K, V> extends HashTable<K, V> {
+    private static final int DEFAULT_CAPACITY = 22;
+    private static final float DEFAULT_LOAD_FACTOR = 0.6f;
+
+
+    /**
+     * Default constructor
+     */
+    HashTableOpenAddressing() {
+        this(DEFAULT_CAPACITY, DEFAULT_LOAD_FACTOR);
+    }
+
+    /**
+     * Constructor with given capacity
+     *
+     * @param capacity int
+     */
+    HashTableOpenAddressing(int capacity) {
+        this(capacity, DEFAULT_LOAD_FACTOR);
+    }
+
+    /**
+     * Constructor with given capacity and load factor
+     *
+     * @param capacity   int
+     * @param loadFactor float
+     */
+    HashTableOpenAddressing(int capacity, float loadFactor) {
+        this.capacity = capacity;
+    }
+
     @Override
     public void add(K key, V value) {
 
