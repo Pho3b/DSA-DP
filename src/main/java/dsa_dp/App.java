a129c4ac3da702aa37f69228023cfed8644cce55
a-rinaldi <andrea.rinaldi54@gmail.com>
2022-06-22 23:42:37 +0200

    feat: implemented the puzzle createSpiral algorithm

--- src/main/java/dsa_dp/App.java
+++ src/main/java/dsa_dp/App.java
@@ -1,26 +1,16 @@
 package dsa_dp;
 
-import dsa_dp.data_structures.graph.AdjacencyListGraph;
+import dsa_dp.algorithms.puzzle.ClockWiseSpiral;
+
+import java.util.Arrays;
 
 public class App {
 
     public static void main(String[] args) {
         System.out.println("This is your current operating system: " + System.getProperty("os.name") + "\n");
-        currentWorkingOnIssueTest();
-    }
 
-    public static void currentWorkingOnIssueTest() {
-        AdjacencyListGraph graph = new AdjacencyListGraph(5);
-        graph.addWeightedEdge(0, 1, 4);
-        graph.addWeightedEdge(0, 2, 1);
-        graph.addWeightedEdge(2, 1, 2);
-        graph.addWeightedEdge(1, 3, 1);
-        graph.addWeightedEdge(3, 4, 3);
-        graph.addWeightedEdge(2, 3, 5);
-
-        System.out.println(graph.djikstraShortestPath(0, 3));
-        System.out.println(graph.djikstraShortestPath(1, 0));
-        System.out.println(graph.djikstraShortestPath(2, 4));
-        System.out.println(graph.djikstraShortestPath(4, 3));
+        // Your code here
+        ClockWiseSpiral cws = new ClockWiseSpiral();
+        System.out.println(Arrays.deepToString(cws.createSpiral(4)));
     }
 }
---
+++ src/main/java/dsa_dp/algorithms/puzzle/ClockWiseSpiral.java
@@ -0,0 +1,53 @@
+package dsa_dp.algorithms.puzzle;
+
+public class ClockWiseSpiral {
+
+    /**
+     * The objective of this function is to receive an integer N and return an NxN two-dimensional array
+     * with numbers 1 through NxN represented as a clockwise spiral.
+     *
+     * Example:
+     * N = 3 Output: [[1,2,3],[8,9,4],[7,6,5]]
+     * 1    2    3
+     * 8    9    4
+     * 7    6    5
+     *
+     * @param N matrix side length
+     * @return the newly generated matrix or an empty one if the given 'N' is less than 1
+     */
+    public int[][] createSpiral(int N) {
+        if (N < 1)
+            return new int[][]{};
+
+        int x = 0;
+        int y = 0;
+        int offset = 0;
+        int circleCounter = 1;
+        int[][] res = new int[N][N];
+
+        for (int i = 1; i < (N * N) + 1; i++) {
+            int revOffset = (N - offset) - 1;
+            res[y][x] = i;
+            circleCounter++;
+
+            // Updating coordinates according to the current position
+            if (y == offset && x < revOffset) {
+                x++;
+            } else if (x == revOffset && y < revOffset) {
+                y++;
+            } else if (y == revOffset && x > offset) {
+                x--;
+            } else if (x == offset && y > offset) {
+                y--;
+            }
+
+            // Checking if we completed one clockwise circle
+            if (circleCounter == (revOffset - offset) * 4) {
+                offset++;
+                circleCounter = 0;
+            }
+        }
+
+        return res;
+    }
+}
