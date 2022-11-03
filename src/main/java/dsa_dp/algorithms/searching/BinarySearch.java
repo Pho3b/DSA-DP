20fe01f5c3309298ba962ab5dfa879e137b837e7
arinaldi <andrea.rinaldi54@gmail.com>
2022-11-03 22:33:37 +0100

    style: minor BS code style update

--- src/main/java/dsa_dp/algorithms/searching/BinarySearch.java
+++ src/main/java/dsa_dp/algorithms/searching/BinarySearch.java
@@ -3,16 +3,13 @@ package dsa_dp.algorithms.searching;
 public class BinarySearch {
 
     public int search(int[] inputArr, int toSearch) {
-        int r = inputArr.length - 1;
         int l = 0;
-        int mid;
+        int r = inputArr.length - 1;
 
         while (l <= r) {
-            mid = l + (r - l) / 2;
+            int mid = l + (r - l) / 2;
 
-            if (toSearch == inputArr[mid])
-                return mid;
-
+            if (toSearch == inputArr[mid]) return mid;
             if (toSearch < inputArr[mid]) r = mid - 1;
             else l = mid + 1;
         }
