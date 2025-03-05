6c82dbe2d5c780a20a7e2ba1cf69ed21842a4c32
arinaldi <andrea.rinaldi2204@gmail.com>
2025-03-05 09:07:08 +0100

    chore: removes unused main App class

--- src/main/java/dsa_dp/App.java
+++
@@ -1,8 +0,0 @@
-package dsa_dp;
-
-public class App {
-
-    public static void main(String[] args) {
-        System.out.println("This is your current operating system: " + System.getProperty("os.name") + "\n");
-    }
-}
